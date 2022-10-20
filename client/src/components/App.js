5d4eb945b64b5cac3b3ff093d2130c1beacfbb04
Salome <sallynanjala254@gmail.com>
2022-10-20 18:57:14 +0300

    Modidfied my footer.js and App.js and Added react-icons module to my app

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="bg-green-400 w-full h-full">
       <h1 className="text-red-500">Hello</h1>
+      <Footer/>
     </div>
   );
 }
---
+++ client/src/components/Footer.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import {BsFacebook,BsYoutube} from 'react-icons/bs'
+import {AiFillInstagram,AiFillTwitterCircle} from 'react-icons/ai'
+
+const Footer = () => {
+  return (
+    <footer className='w-full bg-black'>
+      <div className='lg:w-3/5 w-11/12 m-auto p-5 flex justify-between'>
+        <div className='flex flex-col space-y-2'>
+          <h1 className='text-xl font-bold text-white'>Links</h1>
+          <a className='text-xs text-white' href="/">Home</a>
+          <a className='text-xs text-white' href="/">Shop</a>
+          <a className='text-xs text-white' href="/">About</a>
+          <a className='text-xs text-white' href="/">Contact</a>
+          
+        </div>
+        <div className='flex flex-col space-y-2'>
+          <h1 className='text-xl font-bold text-white'>Help</h1>
+          <a className='text-xs text-white' href="/">Payment Options</a>
+          <a className='text-xs text-white' href="/">Returns</a>
+          <a className='text-xs text-white' href="/">Private Policies</a>
+       
+        </div>
+        <div className='flex flex-col space-y-2'>
+          <h1 className='text-xl font-bold color: text-white'>Newsletters</h1>
+          <a className='text-xs text-white text-white' href="/">Enter your Email Address</a>
+          <a className='text-xs text-white text-white' href="/">Subscribe</a>
+          
+          
+        </div>
+        <div className='flex flex-col space-y-2'>
+          <h1 className='text-xl font-bold text-white'>Socials</h1>
+          <BsFacebook size={'2rem'} style={{color: "#FFF"}}/>
+        <BsYoutube size={'2rem'} style={{color: "#FFF"}}/>
+        <AiFillInstagram size={'2rem'} style={{color: "#FFF"}}/>
+        <AiFillTwitterCircle size={'2rem'} style={{color: "#FFF"}}/> 
+        </div>
+      </div>
+      <div className='w-1/2 m-auto p-5 flex justify-center space-x-10 text-gray-700'>
+         
+      <h1 className='text-sl font-bold text-white'>2022 Shop-It.All rights Reserved;</h1>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;

ec17702775fbebc6b1f6bee6dcffb592a4d3a995
Salome <sallynanjala254@gmail.com>
2022-10-21 00:14:44 +0300

    Modidfied the About.js file

--- client/src/components/AboutUs.js
+++ client/src/components/AboutUs.js
@@ -3,31 +3,39 @@ import imageabout from '../images/imageabout.png';
 
 function AboutUs() {
   return (
-    <div className='w-full bg-white text-center bg-no-repeat bg-center h-500
+    <div className=' bg-white
+        text-center  bg-center h-500 
     '>
-       <div className='w-full bg-white text-center bg-no-repeat bg-center h-50 w-70
+    <div className=' bg-white
+        text-center  bg-center h-50 w-70 bg-left-top
     '> 
       <h1 className='text-2xl font-bold text-black bg-center'>About Us</h1>
-      <br/>
-        <img src={imageabout} height={10} width={2000} />  
+        <br/>
+        <img src={imageabout}  alt={"loading"}/>  
     </div>
       <br/>
+      <div className=' bg-slate-200
+        text-center  bg-center  
+    '> 
       
       <p class="text-black text-bold, center"></p>
       <br />
       <p class="text-black">
       <h1 className='text-xl font-bold text-black'>Shop-It</h1>
-      We are the best online shopping center, 
-      with all the good and amazing products 
-      you need with just 
-       a click of a button. 
-       Make sure to share and subscribe  on
+      We are the best online shopping center,<br /> 
+      with all the good and amazing products <br />
+      you need with just <br />
+       a click of a button. <br />
+       Make sure to share and subscribe <br /> on
       all our social media pages
       Get  more Today!!
       </p>
       
       <br />
-      
+      </div>
+      <div className=' bg-slate-200
+        text-center  bg-center  
+    '> 
       <p class="text-black">
       <h1 className='text-xl font-bold text-black'>Mission</h1>
       We help people achieve independence <br />by making it easier to start,<br/>
@@ -37,20 +45,28 @@ function AboutUs() {
       </p>
       
       <br />
-      
+      </div>
+      <div className=' bg-slate-200
+        text-center  bg-center  
+    '> 
       <p class="text-black">
       <h1 className='text-xl font-bold text-black'>Vision</h1>
-      To be the leading online shopping center in Africa 
+      To be the leading online <br />shopping center <br />in Africa 
       and beyond
       </p>
     
       <br />
-     
+     </div>
+
+
+     <div className=' bg-slate-200 text-center  bg-center  '> 
       <p class="text-black text-center">
+      
       <h1 className='text-xl font-bold text-black'>Our Values</h1>
-      Loyalty<br />Honesty<br />Trust<br />Ingenuity<br />Accountability<br />
-      Simplicity,Respect And Value-centricity
+      Loyalty, Honesty, Trust<br />Ingenuity,Accountability<br />
+      Simplicity Respect And Value-centricity
       </p>
+      </div>
     </div>
   )
 }

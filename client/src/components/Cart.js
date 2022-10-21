5e6ddec45ea034d0c28c1b9eda9f93633d762dde
Kenneth Albert <gichuka@gmail.com>
2022-10-21 17:49:04 +0300

    Done with the Navbar

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -1,8 +1,63 @@
-import React from 'react'
+import NavBar from "./NavBar"
+
 
 function Cart() {
   return (
-    <div>Cart</div>
+<>
+
+<NavBar/>
+
+<div className='flex justify-between  mx-7  font-serif ' >
+    <div className='flex flex-col w-full h-fit relative '>
+
+     <div className='bg-slate-100	pl-4 w-4/5 h-96 mb-6 rounded-2xl shadow-2xl'>
+     <img src="https://raw.githubusercontent.com/Elvis-Munene/Abojani-Academy-web-app/master/assets/images/Rectangle%2010.png"  
+     
+     width="250" height="150" 
+     alt=""  className='mt-8 ml-2 hover:scale-75'/>
+
+    <p className='absolute ml-72 top-10'> Women long sleeve shirt</p> 
+    <p className='absolute left-1/2 ml-6 top-10'> 1</p> 
+    <p className='absolute left-1/2 ml-24 top-10'>Ksh  3000</p> 
+    <p className='absolute left-1/2 ml-48 top-10'>Ksh 3000</p> 
+
+
+<button className='bg-[color:red] font-semibold p-2.5 border-none rounded-md w-48 h-12  ml-1 mt-10 text-sm cursor-pointer hover:bg-red-700 hover:text-black'>
+    REMOVE
+  </button>
+
+   <div className='absolute left-2/4 top-60 '>
+   <button className='bg-[color:red] font-semibold p-2.5 border-none rounded-md w-12 h-12 mr-4  text-sm cursor-pointer hover:bg-red-700 hover:text-black'>
+     +</button>
+      1
+ <button className='bg-[color:red] font-semibold p-2.5 border-none  rounded-md w-12 h-12 ml-4  text-sm cursor-pointer hover:bg-red-700 hover:text-black'>
+ - </button>
+
+   </div>
+
+     </div>
+
+
+     
+
+
+
+    </div>
+
+
+
+<div className='bg-black w-80 h-80 text-[color:white] rounded-xl shadow-2xl font-bold'>
+<h5 className='ml-14 mt-16 text-base'> CART SUMMARY </h5>
+<h6 className='ml-16 mt-14 text-xs'>SubTotal Ksh3,000</h6>
+      
+<button className='bg-orange-600 font-semibold p-2.5 border-none rounded-md w-48 h-12  ml-10 mt-20 text-sm cursor-pointer hover:bg-green-500 hover:text-black'>
+ CHECKOUT  Ksh 3,000  </button>
+</div>
+
+
+    </div>
+
+    </>
   )
 }
 
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,59 @@
+import { CiSearch } from "react-icons/ci";
+import {RiAccountPinBoxFill} from "react-icons/ri";
+import {IoMdHelpCircle} from "react-icons/io";
+import {FaShoppingCart} from "react-icons/fa"
+import { Link} from "react-router-dom";
+import piclogo from "./images/logo2.jpg";
+
+
+function NavBar() {
+  return (
+    <div className=' sticky top-0 flex flex-col w-full h-32   mb-24 font-serif '>
+<div className='w-full h-1/2 bg-slate-50 flex justify-between'>
+
+  <img src={piclogo}  width="45"  alt="logo" className="hover:scale-125 ml-4" />
+    <h3 className=" absolute  top-2.5  font-extrabold italic text-orange-900  ml-16  mt-1"> Shop It</h3>
+
+   <div className=' mt-1 absolute left-40 ' > 
+   <button><CiSearch size={28} className=' absolute top-2 mr-20 hover:scale-125 hover:text-slate-600 cursor-pointer '/></button>
+   <input  className=" p-1 border w-96 text-center  border-slate-500 rounded-lg" placeholder="Search products, brands, and categories" type="text"/>
+   <button className='bg-black  ml-2 mt-1 w-20 h-8 text-[color:white] rounded-md hover:underline hover:scale-75'> Search</button>
+  </div>
+
+
+<div>
+  <RiAccountPinBoxFill size={28} className="absolute left-2/4 ml-10 top-2.5 hover:scale-125 hover:text-slate-600 cursor-pointer "/>
+  <h3 className="absolute left-2/4 ml-20 top-2.5 font-bold "> Account</h3>
+</div>
+
+<div>
+<IoMdHelpCircle size={28} className='absolute left-2/4 ml-52 top-2.5 hover:scale-125 hover:text-slate-600 cursor-pointer'/>
+</div>
+
+<div>
+
+  <FaShoppingCart size={28} className='mr-20 mt-2.5 hover:scale-125 hover:text-slate-600 cursor-pointer '/>
+  <h3 className="absolute top-2.5 ml-10 font-bold "> Cart</h3>
+</div>
+
+</div >
+
+
+<div  className='w-full h-20 text-[color:white] bg-black flex  justify-between items-center'>
+            <Link className="ml-5 hover:underline ">Electronics</Link>
+
+          <Link className="hover:underline">Food Stuffs</Link>
+          <Link className="hover:underline">Fashion</Link>
+          <Link className="hover:underline">Books</Link>
+          <Link className="hover:underline">Health&Beauty</Link>
+          <Link className="hover:underline">Appliances</Link>
+          <Link className="hover:underline">Phones&Tablets</Link>
+          <Link className="hover:underline">Sporting Goods</Link>
+          <Link className="mr-5 hover:underline">Baby Products</Link>
+</div>
+  
+</div>
+  )
+}
+
+export default NavBar

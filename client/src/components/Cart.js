dab82d3a79216598cafb3e65c329e22437e2f18e
Kenneth Albert <gichuka@gmail.com>
2022-10-21 22:51:09 +0300

    updated Navbar & Cart components

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -10,27 +10,27 @@ function Cart() {
 <div className='flex justify-between  mx-7  font-serif ' >
     <div className='flex flex-col w-full h-fit '>
 
-     <div  style={{width:'70%'}} className='bg-slate-100	pl-4  h-96 mb-6 rounded-2xl shadow-2xl relative'>
+     <div  style={{width:'65%'}} className='bg-slate-100	pl-4  h-96 mb-6 rounded-2xl shadow-2xl relative'>
      <img src="https://raw.githubusercontent.com/Elvis-Munene/Abojani-Academy-web-app/master/assets/images/Rectangle%2010.png"  
      
      width="250" height="150" 
      alt=""  className='mt-8 ml-2 hover:scale-75'/>
 
-    <p className='absolute left-72 top-10'><span className="font-semibold ml-4"> Product</span><br/> Women long sleeve shirt</p> 
-    <p className='absolute left-1/2 ml-24 top-10'><span className="font-semibold ">Quantity</span><br/> <span className="ml-4"> 1</span></p> 
-    <p className='absolute left-1/2 ml-48 top-10'><span className="font-semibold">Price</span><br/> Ksh  3,000</p> 
-    <p className='absolute left-1/2 ml-72 top-10'><span className="font-semibold">SubTotal</span><br/>Ksh 3,000</p> 
+    <p className='absolute left-72 top-10'><span className="font-semibold ml-4"> Product</span><br/><br/> Women long sleeve shirt</p> 
+    <p className='absolute left-1/2 ml-24 top-10'><span className="font-semibold ">Quantity</span><br/><br/> <span className="ml-4 "> 1</span></p> 
+    <p className='absolute left-1/2 ml-48 top-10'><span className="font-semibold">Price</span><br/><br/> Ksh  3,000</p> 
+    <p className='absolute left-1/2 ml-72 top-10'><span className="font-semibold">SubTotal</span><br/><br/>Ksh 3,000</p> 
 
 
-<button className='bg-[color:red] font-semibold p-2.5 border-none rounded-md w-48 h-12  ml-1 mt-10 text-sm cursor-pointer hover:bg-red-700 hover:text-black'>
+<button className='bg-[color:#EA4335] font-semibold p-2.5 border-none rounded-md w-48 h-12  ml-1 mt-10 text-sm cursor-pointer hover:bg-red-700 hover:text-black'>
     REMOVE
   </button>
 
    <div className='absolute left-2/4 top-60 '>
-   <button className='bg-[color:green] font-semibold p-2.5 border-none rounded-md w-12 h-12 mr-4  text-sm cursor-pointer hover:bg-green-500 hover:text-black'>
+   <button className='bg-[color:black] text-[color:white] font-extrabold p-2.5 border-none rounded-md w-12 h-12 mr-4  text-sm cursor-pointer hover:scale-125 '>
      +</button>
       1
- <button className='bg-[color:red] font-semibold p-2.5 border-none  rounded-md w-12 h-12 ml-4  text-sm cursor-pointer hover:bg-red-400 hover:text-black'>
+ <button className='bg-[color:black] font-semibold font-extrabold text-[color:white] p-2.5 border-none  rounded-md w-12 h-12 ml-4  text-sm cursor-pointer hover:scale-125'>
  - </button>
 
    </div>
@@ -40,15 +40,10 @@ function Cart() {
 
 
 
-
- 
-
+</div>
 
 
 
-
-
-    </div>
 
 
 
@@ -56,7 +51,7 @@ function Cart() {
 <h5 className='ml-16 mt-16 text-base'> CART SUMMARY </h5>
 <h6 className='ml-20 mt-14 text-sm'>SubTotal  Ksh 3,000</h6>
       
-<button className='bg-orange-600 font-semibold p-2.5 border-none rounded-md w-48 h-12  ml-10 mt-20 text-sm cursor-pointer hover:bg-green-500 hover:text-black'>
+<button className='bg-[color:#EA4335] font-semibold p-2.5 border-none rounded-md w-48 h-12  ml-10 mt-20 text-sm cursor-pointer hover:bg-red-700 hover:text-black'>
  CHECKOUT  Ksh 3,000  </button>
 </div>
 
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,7 +3,6 @@ import {RiAccountPinBoxFill} from "react-icons/ri";
 import {IoMdHelpCircle} from "react-icons/io";
 import {FaShoppingCart} from "react-icons/fa"
 import { Link} from "react-router-dom";
-import piclogo from "./images/logo2.jpg";
 
 
 function NavBar() {
@@ -11,10 +10,8 @@ function NavBar() {
     <div className=' sticky top-0 flex flex-col w-full h-32   mb-10 font-serif z-10 shadow-lg'>
 <div className='w-full h-1/2 bg-slate-50 flex justify-between'>
 
-  <img src={piclogo}  width="45"  alt="logo" className="hover:scale-125 ml-4" />
-    <h3 className=" absolute  top-2.5  font-extrabold italic text-orange-900  ml-16  mt-1"> Shop It</h3>
 
-   <div className=' mt-1 absolute left-40 ' > 
+   <div className=' mt-1 absolute left-72 ' > 
    <button><CiSearch size={28} className=' absolute top-2 mr-20 hover:scale-125 hover:text-slate-600 cursor-pointer '/></button>
    <input  className=" p-1 border w-96 text-center  border-slate-500 rounded-lg" placeholder="Search products, brands, and categories" type="text"/>
    <button className='bg-black  ml-2 mt-1 w-20 h-8 text-[color:white] rounded-md hover:underline hover:scale-75'> Search</button>
@@ -22,17 +19,18 @@ function NavBar() {
 
 
 <div>
-  <RiAccountPinBoxFill size={28} className="absolute left-2/4 ml-20 top-2.5 hover:scale-125 hover:text-slate-600 cursor-pointer "/>
-  <h3 className="absolute left-2/4 ml-28 top-2.5 font-bold "> Account</h3>
+  <RiAccountPinBoxFill size={28} className="absolute left-2/4 ml-64 top-2.5 hover:scale-125 hover:text-slate-600 cursor-pointer "/>
+  <h3 className="absolute left-2/4 ml-72 top-2.5 font-bold "> Account</h3>
 </div>
 
 <div>
-<IoMdHelpCircle size={28} className='absolute left-2/4 ml-72 top-2.5 hover:scale-125 hover:text-slate-600 cursor-pointer'/>
+<IoMdHelpCircle size={28} className='absolute left-2/4 ml-96 top-2.5 hover:scale-125 hover:text-slate-600 cursor-pointer'/>
+<h3 className="absolute top-2.5 right-48 font-bold "> Help</h3>
 </div>
 
 <div>
 
-  <FaShoppingCart size={28} className='mr-20 mt-2.5 hover:scale-125 hover:text-slate-600 cursor-pointer '/>
+  <FaShoppingCart size={28} className='mr-20 mt-2.5  ml-2 hover:scale-125 hover:text-slate-600 cursor-pointer '/>
   <h3 className="absolute top-2.5 ml-10 font-bold "> Cart</h3>
 </div>
 

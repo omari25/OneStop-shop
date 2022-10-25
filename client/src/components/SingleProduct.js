9c79387cd84d5568ba7236e464753032284138f5
georgekinyanjui1 <george.kinyanjui@student.moringaschool.com>
2022-10-25 16:20:20 +0300

    modified the SingleProduct

--- client/src/components/SingleProduct.js
+++ client/src/components/SingleProduct.js
@@ -25,3 +25,23 @@
 // }
 
 // export default SingleProduct
+
+
+import react from "react";
+import product from '../data'
+
+function Singleproduct(){
+const{productId}=useParams();
+const product=product.find((product)=>product.id===productId);
+return(
+<section className="section product">
+<img src={image} alt={shop.title}/>
+<h3>{shop.title}</h3>
+<p>Unit Price:Ksh{shop.price}</p>
+<button>Add to Cart</button>
+<Link to='/products'>back to products</Link>
+</section>
+);
+};
+
+export function Singleproduct()

2fd9b4246d7116d6e13ea5b20906ae26bd6db8aa
Esther Camilla Akinyi <camillaodhiambo8@gmail.com>
2022-10-26 11:39:01 +0300

    Updated associations

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :products, only: [:index, :show]
-  resources :categiries, only: [:index, :show]
+  resources :categories, only: [:index, :show]
 end
 

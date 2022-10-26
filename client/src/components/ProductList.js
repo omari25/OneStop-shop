c8d6a1789172c20b9605b2ecb54bac436344fd56
Esther Camilla Akinyi <camillaodhiambo8@gmail.com>
2022-10-26 11:19:38 +0300

    created a new component

---
+++ client/src/components/ProductList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import FeaturedTitle from './FeaturedTitle'; 
+import ProductCard from './ProductCard';
+
+
+function ProductList({ heading, data }) {
+  return (
+    <div className="xl:px-0 px-2">
+      <div className="flex justify-between items-center py-5">
+        <FeaturedTitle text={heading} />
+        <button className="text-red-600 font-semibold">View All</button>
+      </div>
+      <div className="grid sm:grid-cols-2 md:grid-cols-4 gap-4">
+        {data.map((product) => {
+          return <ProductCard key={product.id} item={product} />;
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default ProductList;

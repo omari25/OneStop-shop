f7c4df73a6cb24ce5bc89e6afb95768f0f7d4cba
Esther Camilla Akinyi <camillaodhiambo8@gmail.com>
2022-10-26 11:17:32 +0300

    created a fetch request

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import data from '../data';
+// import data from '../data';
 import Slider from './Slider';
 import FeaturedTitle from './FeaturedTitle';
+import ProductList from './ProductList';
 // import electronicsdata from '../electronicsdata';
 // import fooddata from '../fooddata';
 // import sportsdata from '../sportsdata';
@@ -19,16 +20,18 @@ function Home( ){
   // const [sportingGoods, setSportingGoods] = useState([])
   // const [babyProducts, setbabyProducts] = useState([])
 
-  const fetchCategories = async () => {
-    const { data } = await axios.get("/categories");
-    const { electronics, foodStuffs, fashion, books, health } = data.products;
-    setElectronics(electronics);
-    setFoodStuffs(foodStuffs);
-    setFashion(fashion);
-    setBooks(books);
-    setHealth(health);
+  // const fetchCategories = async () => {
+  
+    // const { data } = await axios.get("/category/2");
+    // const  foodStuffs   = data.products;
+    // // setElectronics(electronics);
+    // setFoodStuffs(foodStuffs);
+    // // setFashion(fashion);
+    // // setBooks(books);
+    // // setHealth(health);
+    //  console.log(foodStuffs)
 
-  };
+  // };
 
   // function handleCategory() {
   //   fetch('/products', { method: 'GET' }).then ((r) => {
@@ -39,10 +42,22 @@ function Home( ){
   //   });
   // }
 
-  useEffect(() => {
-    fetchCategories();
+  // useEffect(() => {
+  //   fetchCategories();
    
-  }, []);
+  // }, []);
+
+  useEffect(() => {
+            fetch('categories/2')
+            .then(response => response.json())
+            .then((data) => {
+            setFoodStuffs(data.products)
+            console.log(data.products)
+            })
+        }, [])
+
+        const newFoodStuffs = foodStuffs.slice(0, 6)
+        console.log(newFoodStuffs)
 
   return (
     
@@ -53,24 +68,33 @@ function Home( ){
 
       
       
-        <FeaturedTitle className=" py-3" text= "Shop by Categories" />
+      <FeaturedTitle className=" py-3" text= "Shop by Category" />
 
         <div className="flex flex-row justify-between items-center px-4 py-4  h-60">
-          {data.map((item) => (
-            <div key={item.id} className="one item div w-80 h-50">
-              <img classname="rounded-md" src={item.img} alt=""/>
-              <p className="font-light">{item.name}</p>
-              <p className="font-medium">{item.price}</p>
-        
+        <div className="sm:w-2/4">
+            <div className=" w-full aspect-auto aspect-h-9  rounded-md ">
+              <img 
+              src="https://img.freepik.com/free-vector/black-friday-sale-with-realistic-3d-paper-page_1361-3675.jpg?w=1380&t=st=1666600766~exp=1666601366~hmac=fcde90b5347db30e1d9353087909bef7a1128fd62740b2ec6d2cdd3cf04ff6fb" alt="" />
             </div>
-          ))}
+          </div>
+          <div className="sm:w-2/4">
+            <div className="aspect-w-16 aspect-h-9">
+              <img src="" alt="" />
+            </div>
+          </div>
         </div>
       </div>
-    
+
+      <ProductList heading="Electronics" data={electronics} />
+      <ProductList heading="Food Stuffs" data={newFoodStuffs} />
+      <ProductList heading="Fashion" data={fashion} />
+      <ProductList heading="Books" data={books} />
+      <ProductList heading="Health" data={health} />
+            
    
    </>
   );
-}
+  }
 
 export default Home;
 

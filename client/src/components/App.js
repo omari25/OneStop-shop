411f3e1878e052ee84d9c3e4500c50ac8fbe6863
Esther Camilla Akinyi <camillaodhiambo8@gmail.com>
2022-10-26 15:43:30 +0300

    Updated

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,14 +4,14 @@ import AboutUs from "./AboutUs";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import Cart from "./Cart"
-import Electronics from "./Electronics";
-import FoodStuffs from "./FoodStuffs";
-import Books from "./Books";
-import Phones from "./Phones";
-import Fashion from "./Fashion";
-import Beauty from "./Beauty";
-import BabyProducts from "./BabyProducts";
-import SportingGoods from "./SportingGoods";
+// import Electronics from "./Electronics";
+// import FoodStuffs from "./FoodStuffs";
+// import Books from "./Books";
+// import Phones from "./Phones";
+// import Fashion from "./Fashion";
+// import Beauty from "./Beauty";
+// import BabyProducts from "./BabyProducts";
+// import SportingGoods from "./SportingGoods";
 // import Login from "./LoginForm";
 // import Signup from "./SignupForm";
 
@@ -33,14 +33,14 @@ function App() {
           <Route exact path="/" element={<Home/>} />
           <Route exact path="/cart"  element={<Cart/>} />
           <Route exact path="/aboutus"  element={<AboutUs/>} />
-          <Route exact path="/electronics"  element={<Electronics/>} />
+          {/* <Route exact path="/electronics"  element={<Electronics/>} />
           <Route exact path="/foodstuffs"  element={<FoodStuffs/>} />
           <Route exact path="/fashion"  element={<Fashion/>} />
           <Route exact path="/books"  element={<Books/>} />
           <Route exact path="/beauty"  element={<Beauty/>} />
           <Route exact path="/phones"  element={<Phones/>} />
           <Route exact path="/sporting-goods"  element={<SportingGoods/>} />
-          <Route exact path="/baby-products"  element={<BabyProducts/>} />
+          <Route exact path="/baby-products"  element={<BabyProducts/>} /> */}
         </Routes>
       </div>
   );

7517de01cb4e889cd7948db95cd65e3650c73b30
Omar Mahat <omar.mahat@student.moringaschool.com>
2022-10-26 09:58:54 +0300

    Resolve conflict in the Gemfile.lock

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_10_24_064201) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -66,7 +65,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_24_064201) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-
 
 end

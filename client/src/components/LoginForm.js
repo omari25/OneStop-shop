08cd14c4e15d2232d3e624e682db9f55eb5ac180
georgekinyanjui1 <george.kinyanjui@student.moringaschool.com>
2022-10-27 08:57:44 +0300

    clear white space

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import pic from "./images/image2.png";
 
+
 function Login({ setUser }) {
   const navigate = useNavigate();
 

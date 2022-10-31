39da10c4e10f6a032dc9726d86d4e735791c30d2
Kenneth Albert <gichuka@gmail.com>
2022-10-31 10:22:22 +0300

    updated paypal

--- db/migrate/20221020155534_create_users.rb
+++ db/migrate/20221020155534_create_users.rb
@@ -15,4 +15,3 @@ class CreateUsers < ActiveRecord::Migration[7.0]
     end
   end
 end
-m
--- db/schema.rb
+++ db/schema.rb
@@ -61,6 +61,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_24_184028) do
     t.string "last_name"
     t.string "email"
     t.integer "phone_number"
+    t.string "county"
+    t.string "city"
+    t.string "address"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

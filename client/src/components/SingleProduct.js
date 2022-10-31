c3466731aeeb8d73503aaafcfab7aa7cd846c9a1
georgekinyanjui1 <george.kinyanjui@student.moringaschool.com>
2022-10-31 07:17:30 +0300

    modified single product

--- client/src/components/SingleProduct.js
+++ client/src/components/SingleProduct.js
@@ -1,27 +1,59 @@
-// import { useState, useEffect } from "react"
-// import { useParams } from "react-router-dom"
+import { useState, useEffect } from "react"
+import { useNavigate, useParams } from "react-router-dom"
+import { CartContext } from "../CartContext";
+import { useContext } from "react";
 
-// function SingleProduct(){
-//     const { id } = useParams
-//     const [ product, setProduct ] = useState([])
+    // qali2244
+
+function SingleProduct({user}){
+    const { id } = useParams()
+    const [ product, setProduct ] = useState([])
+    const navigate = useNavigate()
+
+    const cart = useContext(CartContext);
     
-//     useEffect(() => {
-//         fetch(`products`)
-//         .then(response => response.json())
-//         .then((data) => {
-//         setProduct(data)
-//         })
-//     }, [])
+    useEffect(() => {
+        fetch(`/products/${id}`)
+        .then(response => response.json())
+        .then((data) => {
+        setProduct(data)
+        })
+    }, [])
 
-//     return (
-//         <div>
-//             {
-//                 product.map((pro)=>(
-//                     pro[id].product_name
-//                 ))
-//             }
-//         </div>
-//     )
-// }
+    function handleUser(){
+        if (user){
+          navigate("/checkout")
+        }
+        else{
+          navigate("/account")
+        }
+    }
 
-// export default SingleProduct
+    return (
+        <div>
+            {
+                <div className="mx-16 flex justify-around mb-8">
+
+                    <div className="w-[40%]">
+                        <img src={product.image_url} className="w-full" alt="" />
+                    </div>
+
+                    <div className="w-[40%]">
+                        <h1 className="text-2xl font-bolded">{product.product_name}</h1>
+                        <h4 className="text-2xl font-bolded mt-4">Price {`Ksh: ${product.price}`}</h4>
+                        <h1 className="text-2xl font-bolded mt-4">Description</h1>
+                        <p>{product.description}</p>
+
+                        <div className="flex justify-between w-full items-center mt-16">
+                            <button onClick={() => {cart.addOneToCart(product.id,product)}} className="bg-[black] text-[white] w-[40%] py-2 rounded-md">Add to cart</button>
+                            <button onClick={handleUser} className="bg-[black] text-[white] w-[40%] py-2 rounded-md" >Buy now</button>
+                        </div>
+                    </div>
+
+                </div>
+            }
+        </div>
+    )
+}
+
+export default SingleProduct
